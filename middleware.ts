4fe7c2c07a384e60c93e7a3cc95efce9fc004149
adishgarg <gargadi456@gmail.com>
2024-09-03 23:26:29 +0530

    init

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  // routes visible to the public without logging in
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
